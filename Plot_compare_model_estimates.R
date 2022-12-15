78810fac85fb800539be586c4ab89c2fdced3da7
Viljugrein <vi1142@vetinst.no>
2022-12-15 10:50:01 +0100

    Update Plot_compare_model_estimates.R
    Supplementary figures updated

--- Plot_compare_model_estimates.R
+++ Plot_compare_model_estimates.R
@@ -1,8 +1,10 @@
+library(grid)
+library(lattice) #utilized to plot multiple plots on a single page
 
 ##Specifying the colors
 red    = "#E27D60"
 blue   = "#085DCB"
-orange = "#E8A87C"
+
 
 ###############################################################################
 ##Comparing baseline model (HV_M2C) with model excluding summer minimum counts (HV_M1C)
@@ -10,9 +12,12 @@ orange = "#E8A87C"
 
 modA<-HV_M2C
 
-png(paste0(pathR,"EffPlot1_phi1_M1M2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
+png(paste0(pathR,"SuppPlot1_v2.png"),width = 7.8, height = 12, units = "in", pointsize = 12,
+    res=1200,
     bg = "white", family = "", restoreConsole = TRUE)
+
+par(mfrow=c(3,2))
+
 MCMCplot(object = HV_M1C, 
          object2 = modA, 
          params = 'phi1', 
@@ -22,12 +27,8 @@ MCMCplot(object = HV_M1C,
          labels=c("2005",
            "2006","2007","2008","2009","2010","2011",
            "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate phi1")
-dev.off()
+           "2018","2019","2020","2021"),xlab="Summer survival of calves (phi1)")
 
-png(paste0(pathR,"EffPlot1_f_M1M2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
-    bg = "white", family = "", restoreConsole = TRUE)
 MCMCplot(object = HV_M1C, 
          object2 = modA, 
          params = 'f', 
@@ -36,57 +37,7 @@ MCMCplot(object = HV_M1C,
          labels=c("2005",
            "2006","2007","2008","2009","2010","2011",
            "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate f")
-dev.off()
-
-png(paste0(pathR,"EffPlot1_p1_M1M2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
-    bg = "white", family = "", restoreConsole = TRUE)
-MCMCplot(object = HV_M1C, 
-         object2 = modA, 
-         params = 'p1', 
-         col2=blue,
-         offset = 0.1,xlim=c(0.4,1),
-         labels=c("2005",
-           "2006","2007","2008","2009","2010","2011",
-           "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate p1")
-dev.off()
-
-png(paste0(pathR,"EffPlot1_p2_M1M2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
-    bg = "white", family = "", restoreConsole = TRUE)
-
-MCMCplot(object = HV_M1C,
-         object2 = modA, 
-         params = 'p2', 
-         offset = 0.1,xlim=c(0,1),
-         col2=blue,
-         labels=c("2005",
-           "2006","2007","2008","2009","2010","2011",
-           "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate p2")
-dev.off()
-
-png(paste0(pathR,"EffPlot1_Xtot_M1M2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
-    bg = "white", family = "", restoreConsole = TRUE)
-
-MCMCplot(object = HV_M1C,
-         object2 = modA, 
-         params = 'Xtot', 
-         col2=blue,
-         offset = 0.1,
-         labels=c("2005",
-           "2006","2007","2008","2009","2010","2011",
-           "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate Xtot")
-dev.off()
-
-
-png(paste0(pathR,"EffPlot1_lambda_M1M2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
-    bg = "white", family = "", restoreConsole = TRUE)
+           "2018","2019","2020","2021"),xlab="Early recruitment rate (f)")
 
 MCMCplot(object = HV_M1C,
          object2 = modA, 
@@ -96,9 +47,40 @@ MCMCplot(object = HV_M1C,
          labels=c(
            "2006","2007","2008","2009","2010","2011",
            "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate lambda")
-dev.off()
+           "2018","2019","2020","2021"),xlab="Population growth rate (λ)")
 
+MCMCplot(object = HV_M1C,
+         object2 = modA, 
+         params = 'Xtot', 
+         col2=blue,
+         offset = 0.1,
+         labels=c("2005",
+                  "2006","2007","2008","2009","2010","2011",
+                  "2012","2013","2014","2015","2016","2017",
+                  "2018","2019","2020","2021"),xlab="Total population size after harvest (Xtot)")
+
+MCMCplot(object = HV_M1C, 
+         object2 = modA, 
+         params = 'p1', 
+         col2=blue,
+         offset = 0.1,xlim=c(0.4,1),
+         labels=c("2005",
+           "2006","2007","2008","2009","2010","2011",
+           "2012","2013","2014","2015","2016","2017",
+           "2018","2019","2020","2021"),xlab="Survey coverage in summer (p1)")
+
+MCMCplot(object = HV_M1C,
+         object2 = modA, 
+         params = 'p2', 
+         offset = 0.1,xlim=c(0,1),
+         col2=blue,
+         labels=c("2005",
+           "2006","2007","2008","2009","2010","2011",
+           "2012","2013","2014","2015","2016","2017",
+           "2018","2019","2020","2021"),xlab="Survey coverage in fall (p2)")
+
+
+dev.off()
 
 
 ###############################################################################
@@ -107,9 +89,12 @@ dev.off()
 modB<-HV_M2C
 modA<-HV_M2CB23phi2
 
-png(paste0(pathR,"EffPlot1_phi1_M2M2phi2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
+png(paste0(pathR,"SuppPlot2_v2.png"),width = 7.8, height = 12, units = "in", pointsize = 12,
+    res=1200,
     bg = "white", family = "", restoreConsole = TRUE)
+
+par(mfrow=c(3,2))
+
 MCMCplot(object = modB, 
          object2 = modA, 
          params = 'phi1',
@@ -119,60 +104,16 @@ MCMCplot(object = modB,
          labels=c("2005",
            "2006","2007","2008","2009","2010","2011",
            "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate phi1")
-dev.off()
+           "2018","2019","2020","2021"),xlab="Summer survival of calves (phi1)")
 
-png(paste0(pathR,"EffPlot1_p1_M2M2phi2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
-    bg = "white", family = "", restoreConsole = TRUE)
-MCMCplot(object = modB, 
-         object2 = modA, 
-         params = 'p1', 
-         col=blue,
-         col2=red,
-         offset = 0.1,xlim=c(0.4,1),
-         labels=c("2005",
-           "2006","2007","2008","2009","2010","2011",
-           "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate p1")
-dev.off()
-
-png(paste0(pathR,"EffPlot1_p2_M2M2phi2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
-    bg = "white", family = "", restoreConsole = TRUE)
-
-MCMCplot(object = modB,
-         object2 = modA, 
-         params = 'p2', 
-         offset = 0.1,xlim=c(0,1),
-         col=blue,
-         col2=red,
-         labels=c("2005",
-           "2006","2007","2008","2009","2010","2011",
-           "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate p2")
-dev.off()
-
-png(paste0(pathR,"EffPlot1_Xtot_M2M2phi2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
-    bg = "white", family = "", restoreConsole = TRUE)
-
-MCMCplot(object = modB,
-         object2 = modA, 
-         params = 'Xtot', 
-         offset = 0.1,
-         col=blue,
-         col2=red,
-         labels=c("2005",
-           "2006","2007","2008","2009","2010","2011",
-           "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate Xtot")
-dev.off()
-
-
-png(paste0(pathR,"EffPlot1_lambda_M2M2phi2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
-    bg = "white", family = "", restoreConsole = TRUE)
+MCMCplot(object = modA,
+         params = 'phi2', xlim=c(0.4,1),
+         offset = 0.1,col=red,
+         labels=c(
+           "2005/06",
+           "2006/07","2007/08","2008/09","2009/10","2010/11","2011/12",
+           "2012/13","2013/14","2014/15","2015/16","2016/17","2017/18",
+           "2018/19","2019/20","2020/21"),xlab="Annual survival of pre-harvest calves to yearlings (phi2)")
 
 MCMCplot(object = modB,
          object2 = modA, 
@@ -183,37 +124,42 @@ MCMCplot(object = modB,
          labels=c(
            "2006","2007","2008","2009","2010","2011",
            "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate lambda")
-dev.off()
-
-png(paste0(pathR,"EffPlot1_f_M2M2phi2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
-    bg = "white", family = "", restoreConsole = TRUE)
+           "2018","2019","2020","2021"),xlab="Population growth rate (λ)")
 
 MCMCplot(object = modB,
          object2 = modA, 
-         params = 'f', 
+         params = 'Xtot', 
+         offset = 0.1,
          col=blue,
          col2=red,
-         offset = 0.1,
+         labels=c("2005",
+                  "2006","2007","2008","2009","2010","2011",
+                  "2012","2013","2014","2015","2016","2017",
+                  "2018","2019","2020","2021"),xlab="Total population size after harvest (Xtot)")
+
+MCMCplot(object = modB, 
+         object2 = modA, 
+         params = 'p1', 
+         col=blue,
+         col2=red,
+         offset = 0.1,xlim=c(0.4,1),
          labels=c("2005",
            "2006","2007","2008","2009","2010","2011",
            "2012","2013","2014","2015","2016","2017",
-           "2018","2019","2020","2021"),xlab="Parameter estimate f")
-dev.off()
+           "2018","2019","2020","2021"),xlab="Survey coverage in summer (p1)")
 
-png(paste0(pathR,"EffPlot1_phi2_M2phi2.png"),width = 6, height = 6, units = "in", pointsize = 12,
-    res=320,
-    bg = "white", family = "", restoreConsole = TRUE)
+MCMCplot(object = modB,
+         object2 = modA, 
+         params = 'p2', 
+         offset = 0.1,xlim=c(0,1),
+         col=blue,
+         col2=red,
+         labels=c("2005",
+           "2006","2007","2008","2009","2010","2011",
+           "2012","2013","2014","2015","2016","2017",
+           "2018","2019","2020","2021"),xlab="Survey coverage in fall (p2)")
 
-MCMCplot(object = modA,
-         params = 'phi2', xlim=c(0.4,1),
-         offset = 0.1,col=red,
-         labels=c(
-           "2005/06",
-           "2006/07","2007/08","2008/09","2009/10","2010/11","2011/12",
-           "2012/13","2013/14","2014/15","2015/16","2016/17","2017/18",
-           "2018/19","2019/20","2020/21"),xlab="Parameter estimate phi2")
+
 dev.off()
 
 ###############################################################################
